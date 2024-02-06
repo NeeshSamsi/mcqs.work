732a3ee54edcd9f7b81551972f908095d249a812
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-02-06 17:58:22 +0530

    feat: Cleanup, add font, add forms plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import forms from "@tailwindcss/forms"
+import theme from "tailwindcss/defaultTheme"
 
 const config: Config = {
   content: [
@@ -8,13 +10,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: ["var(--font-inter)", ...theme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [forms],
+}
+export default config
