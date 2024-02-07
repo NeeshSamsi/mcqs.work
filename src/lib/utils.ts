511e2a351d63fc400398665d580b3f5d114cc203
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-02-07 22:59:13 +0530

    feat: Add numeric enum for zod - not currently supported

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { z, type ZodType } from "zod"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function numericEnum<TValues extends readonly number[]>(
+  values: TValues,
+) {
+  return z.number().superRefine((val, ctx) => {
+    if (!values.includes(val)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_enum_value,
+        options: [...values],
+        received: val,
+      })
+    }
+  }) as ZodType<TValues[number]>
 }
