43571337aa4c2fb9a682562a38ceb34d7c119a23
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-04-18 19:31:11 +0530

    feat: Add icon to props

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,27 +1,15 @@
+import { type ReactNode } from "react"
+
 type Props = {
+  icon: ReactNode
   title: string
   description: string
 }
 
-export default function FeatureCard({ title, description }: Props) {
+export default function FeatureCard({ icon, title, description }: Props) {
   return (
     <div className="block rounded-lg border border-secondary p-8 shadow-lg transition hover:border-primary/30 hover:shadow-primary/30">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="size-10 text-primary"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path d="M12 14l9-5-9-5-9 5 9 5z" />
-        <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-        />
-      </svg>
+      {icon}
       <h2 className="mt-4 text-xl font-bold text-slate-100">{title}</h2>
       <p className="mt-1 text-sm text-slate-400">{description}</p>
     </div>
