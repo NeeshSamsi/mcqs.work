c1da4bc6e450b6597980e4b0ac074bf28b90c66b
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-04-18 16:44:53 +0530

    fix: use esm import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss"
 import { fontFamily } from "tailwindcss/defaultTheme"
 
+import animate from "tailwindcss-animate"
+
 const config = {
   darkMode: ["class"],
   content: [
@@ -78,7 +80,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 } satisfies Config
 
 export default config
