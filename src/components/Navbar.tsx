cdb3d3c1f119bb16bcd1d7f698ba408741934918
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-04-18 18:15:36 +0530

    feat: Styling changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,16 +4,16 @@ import { Button } from "./ui/button"
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between gap-8 py-8">
+    <nav className="mb-8 flex items-center justify-between gap-8 py-8">
       <Link href="/" className="flex items-center gap-2 text-xl">
         <QuestionMarkCircledIcon className="size-7" />
-        <span>MCQ Practice</span>
+        <strong className="font-semibold">mcqs.work</strong>
       </Link>
 
       <div className="flex items-center gap-4">
         <Button asChild>
-          <Link href="/new" className="text-lg">
-            New
+          <Link href="/new" className="text-lg font-semibold">
+            New Session
           </Link>
         </Button>
       </div>
