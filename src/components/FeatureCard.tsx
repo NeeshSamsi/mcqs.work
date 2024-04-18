741cc530fc15b399458ba63c0b6fc41916d81b37
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-04-18 21:46:29 +0530

    feat: Blue icons

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function FeatureCard({ icon, title, description }: Props) {
   return (
     <div className="block rounded-lg border border-secondary p-8 shadow-lg transition hover:border-primary/30 hover:shadow-primary/30">
-      {icon}
+      <div className="text-primary">{icon}</div>
       <h2 className="mt-4 text-xl font-bold text-slate-100">{title}</h2>
       <p className="mt-1 text-sm text-slate-400">{description}</p>
     </div>
