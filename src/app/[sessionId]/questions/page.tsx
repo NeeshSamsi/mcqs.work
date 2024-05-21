63068f203c2aa2f9ae59e8a6b5372c8cac223453
Neesh <avaneeshsamsi@gmail.com>
2024-05-21 16:49:11 +0530

    test: url params and access id

--- src/app/[sessionId]/questions/page.tsx
+++ src/app/[sessionId]/questions/page.tsx
@@ -2,12 +2,11 @@ type Props = {
   params: { sessionId: string }
 }
 
-export default function Answers({ params }: Props) {
-  console.log(params)
-
+export default function Answers({ params: { sessionId } }: Props) {
   return (
     <main>
-      <h1>Answers</h1>
+      <h1>Questions - {sessionId}</h1>
     </main>
+    // <MCQForm />
   )
 }
